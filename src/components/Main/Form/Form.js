071d8dda3f07a0b724eeb73a09cfe9a60cd6d5c7
Zakhar Menshikov <zm681986@mail.ru>
2025-12-03 17:57:53 +0500

    test

--- src/components/Main/Form/Form.js
+++ src/components/Main/Form/Form.js
@@ -5,12 +5,12 @@ function Form() {
     return (
 
         <section className="main-form">
-            <div className="main-form__description">
+            <div className="main-form__description main-show-form">
                 <p className="main-form__description-text">Ваша ситуация не отражена в списке?{'\n'}
                 </p>
                 <p className="main-form__description-text"> Оставьте заявку и мы обязательно Вам поможем!</p>
             </div>
-            <button className="main-form__btn" type="button">
+            <button className="main-form__btn main-show-form" type="button">
                 <span className="btn__text">Оставить заявку</span>
             </button>
         </section>
