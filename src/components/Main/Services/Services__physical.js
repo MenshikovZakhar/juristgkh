224b5e7712ff7dd65f6884e5ef1e81c0c7ddec15
Zakhar Menshikov <zm681986@mail.ru>
2026-01-06 08:31:24 +0500

    test

--- src/components/Main/Services/Services__physical.js
+++ src/components/Main/Services/Services__physical.js
@@ -1,4 +1,9 @@
 import sud from '../../../images/sud.jpg';
+import sud2 from '../../../images/sud-2.png';
+import sud3 from '../../../images/sud-3.png';
+import sud4 from '../../../images/sud-4.png';
+import sud6 from '../../../images/sud-6.png';
+import sud5 from '../../../images/sud-5.jpg';
 import './Services.css';
 import { NavLink, Link } from 'react-router-dom';
 
@@ -15,7 +20,7 @@ function Services__physical() {
                         <div className="catalog__info">
                             <h3 className="catalog__title">Представление интересов в судах</h3>
                             <div className="catalog__mark">
-                                <p className="catalog__price"><span className="catalog__number">5 000</span> руб.</p>
+                                <p className="catalog__price"><span className="catalog__number">от 5 000</span> руб.</p>
                             </div>
                             <div className="btn catalog__btn">
                                 <span className="btn__text">Подробнее</span>
@@ -26,12 +31,12 @@ function Services__physical() {
                 <li className="catalog__item">
                     <Link className="catalog__content" to="/services_1">
                         <div className="catalog__img">
-                            <img src={sud} />
+                            <img src={sud2} />
                         </div>
                         <div className="catalog__info">
-                            <h3 className="catalog__title">Представление интересов в судах</h3>
+                            <h3 className="catalog__title">Отмена судебного приказа </h3>
                             <div className="catalog__mark">
-                                <p className="catalog__price"><span className="catalog__number">5 000</span> руб.</p>
+                                <p className="catalog__price"><span className="catalog__number">от 3 000</span> руб.</p>
                             </div>
                             <div className="btn catalog__btn">
                                 <span className="btn__text">Подробнее</span>
@@ -42,12 +47,12 @@ function Services__physical() {
                 <li className="catalog__item">
                     <Link className="catalog__content" to="/services_1">
                         <div className="catalog__img">
-                            <img src={sud} />
+                            <img src={sud3} />
                         </div>
                         <div className="catalog__info">
-                            <h3 className="catalog__title">Представление интересов в судах</h3>
+                            <h3 className="catalog__title">Списание задолженности по ЖКХ </h3>
                             <div className="catalog__mark">
-                                <p className="catalog__price"><span className="catalog__number">5 000</span> руб.</p>
+                                <p className="catalog__price"><span className="catalog__number">от 5 000</span> руб.</p>
                             </div>
                             <div className="btn catalog__btn">
                                 <span className="btn__text">Подробнее</span>
@@ -58,12 +63,12 @@ function Services__physical() {
                 <li className="catalog__item">
                     <Link className="catalog__content" to="/services_1">
                         <div className="catalog__img">
-                            <img src={sud} />
+                            <img src={sud4} />
                         </div>
                         <div className="catalog__info">
-                            <h3 className="catalog__title">Представление интересов в судах</h3>
+                            <h3 className="catalog__title">Споры с управляющими компаниями и ТСЖ</h3>
                             <div className="catalog__mark">
-                                <p className="catalog__price"><span className="catalog__number">5 000</span> руб.</p>
+                                <p className="catalog__price"><span className="catalog__number">от 5 000</span> руб.</p>
                             </div>
                             <div className="btn catalog__btn">
                                 <span className="btn__text">Подробнее</span>
@@ -74,12 +79,12 @@ function Services__physical() {
                 <li className="catalog__item">
                     <Link className="catalog__content" to="/services_1">
                         <div className="catalog__img">
-                            <img src={sud} />
+                            <img src={sud5} />
                         </div>
                         <div className="catalog__info">
-                            <h3 className="catalog__title">Представление интересов в судах</h3>
+                            <h3 className="catalog__title">Написание запросов и претензий</h3>
                             <div className="catalog__mark">
-                                <p className="catalog__price"><span className="catalog__number">5 000</span> руб.</p>
+                                <p className="catalog__price"><span className="catalog__number">от 2 500</span> руб.</p>
                             </div>
                             <div className="btn catalog__btn">
                                 <span className="btn__text">Подробнее</span>
@@ -90,10 +95,10 @@ function Services__physical() {
                 <li className="catalog__item">
                     <Link className="catalog__content" to="/services_1">
                         <div className="catalog__img">
-                            <img src={sud} />
+                            <img src={sud6} />
                         </div>
                         <div className="catalog__info">
-                            <h3 className="catalog__title">Представление интересов в судах</h3>
+                            <h3 className="catalog__title">Оспаривание результатов общих<br></br>собраний собственников МКД </h3>
                             <div className="catalog__mark">
                                 <p className="catalog__price"><span className="catalog__number">5 000</span> руб.</p>
                             </div>
